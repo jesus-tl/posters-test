15532f844afcdedbcbd6274d6f6676902526da11
jesus-tl <jesus.torres21@upr.edu>
2026-01-30 11:45:13 -0400

    Test 1

--- src/config/serie.config.js
+++ src/config/serie.config.js
@@ -1,21 +1,18 @@
 const config = {
   "lang": "es",
   "localPort": "5173",
-  "base": "https://srsergiorodriguez.github.io",
-  "baseurl": "/serie-mini",
-  "title": "Serie Mini",
-  "subtitle": "Un sistema para mini colecciones digitales",
-  "credits": "Por Sergio Rodríguez Gómez",
-  "copyright": "Todos los derechos reservados, 2024",
-  "logos": [
-    {img: "logos/logo.png", link: "https://github.com/srsergiorodriguez/serie-mini"}
-  ],
+  "base": "https://jesus-tl.github.io",
+  "baseurl": "/posters-test",
+  "title": "Posters",
+  "subtitle": "Coleccion Personal",
+  "credits": "(por Jesus M. Torres Luquis)",
+  "copyright": "Todos los derechos reservados, 2026",
   "pages": {
     "iiifViewer": true,
     "metadataToShow": [
       {
         "key": "label",
-        "label": "Nombre",
+        "label": "Label",
         "type": "text"
       },
       {
